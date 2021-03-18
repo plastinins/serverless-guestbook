fe94a11b248232b04fc0c31158278e619ea07250
plastinins <78303129+plastinins@users.noreply.github.com>
2021-03-18 18:51:19 +0300

    Update guestbook.js
    add api url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://758e0b68.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
